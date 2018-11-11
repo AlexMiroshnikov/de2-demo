520a455fc0cb62c6a9b7ecac877e760d69806a5f
AlexMiroshnikov <ilexicy@gmail.com>
2018-11-11 19:45:20 +0100

    PWA WIP - bump version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '0.0.1-dev-4';
+const VERSION = '0.0.1-dev-5';
 
 const assetsToCache = {
     statics: ['index.html', 'dist/bundle.js', 'css/bootstrap.min.css', 'favicon.ico', 'manifest.json'],
